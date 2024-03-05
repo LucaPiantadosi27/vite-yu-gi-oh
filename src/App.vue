55798ab4abdf24b028b02ac0ebc3435cf549ac39
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-03-05 15:42:36 +0100

    cards on

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script>
 import axios from 'axios';
 import {store} from './store';
+
+import CardsList from './components/CardsList.vue';
 export default {
   data() {
     return {
       store,
     }
+  },
+  components: {
+    CardsList,
   },
   created() {
     axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
@@ -17,7 +22,12 @@ export default {
 }
 </script>
 <template>
-  
+  <div class="container">
+
+    <CardsList></CardsList>
+
+  </div>
 </template>
+
 <style lanc="scss">
 </style>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue'
 
 export const store = reactive({
 
-    // questo oggetto è come se fosse un data() condiviso da tutti i componenti della mia app
+    // |||questo oggetto è come se fosse un data() condiviso da tutti i componenti della mia app|||
     cards: [],
 
 });
